6b6012fa927a535579be45b6ad593f37cc3f0c95
Zheer <bestzheer@gmail.com>
2021-02-22 11:42:34 +0300

    Routing Table Added

--- networking.tf
+++ networking.tf
@@ -35,3 +35,15 @@ resource "aws_internet_gateway" "GW" {
     Name = "GW"
   }
 }
+
+resource "aws_route_table" "RT-Subnet" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.GW.id
+  }
+	tags = {
+	Name = "RT-Subnet"
+	}
+}
