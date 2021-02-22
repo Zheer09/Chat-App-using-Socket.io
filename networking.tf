cb048ce07fb62ebc8b1dcd1e27efd284ff810db2
Zheer <bestzheer@gmail.com>
2021-02-22 11:33:45 +0300

    Subnets added with availability zons

--- networking.tf
+++ networking.tf
@@ -5,3 +5,26 @@ resource "aws_vpc" "main" {
 	Name = "Main"
 	}
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "P-AV1" {
+ 	 vpc_id = aws_vpc.main.id
+ 	 cidr_block = "10.0.1.0/24"
+	 availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = "P-AV1"
+  }
+}
+
+resource "aws_subnet" "P-AV2" {
+         vpc_id = aws_vpc.main.id
+         cidr_block = "10.0.2.0/24"
+         availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name = "P-AV2"
+  }
+}
+
