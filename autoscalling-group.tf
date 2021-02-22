291163c27fc366d56885f92eff38ffdd99ffd72c
Zheer <bestzheer@gmail.com>
2021-02-22 22:08:45 +0300

    Autoscalling-group added

---
+++ autoscalling-group.tf
@@ -0,0 +1,25 @@
+resource "aws_launch_configuration" "OS-Type"{
+	name_prefix = "OS-Type"
+
+	image_id = "ami-0996d3051b72b5b2c"
+	instance_type = "t2.micro"
+	
+	lifecycle {
+	 create_before_destroy = true
+	}
+}
+
+resource "aws_autoscaling_group" "Dynamic-IN"{
+	name = "Dynamic-EC2-instance"
+	min_size = 1
+	max_size = 4
+	desired_capacity = 2
+	health_check_type = "ELB"
+	launch_configuration = aws_launch_configuration.OS-Type.name
+	vpc_zone_identifier = [aws_subnet.P-AV1.id, aws_subnet.P-AV2.id]
+
+	lifecycle {
+	  create_before_destroy = true
+	}
+
+}
