88cfe80c8674150c1454335a7f5e7c98f12ad181
Zheer <bestzheer@gmail.com>
2021-02-28 22:03:09 +0300

    Updated the security group for ssh

--- Application-loadbalancer.tf
+++ Application-loadbalancer.tf
@@ -26,6 +26,13 @@ resource "aws_security_group" "SCG"{
 	  cidr_blocks= ["0.0.0.0/0"]
 	}
 
+	ingress{
+	from_port= 22
+	to_port = 22
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
+
 	egress {
 	 from_port= 0
 	 to_port = 0
