2613a27aebc58e596b168357572fb1e9f10ba457
Zheer <bestzheer@gmail.com>
2021-03-01 20:43:37 +0300

    key name to the lunch configuration added

--- autoscalling-group.tf
+++ autoscalling-group.tf
@@ -5,6 +5,7 @@ resource "aws_launch_configuration" "OS-Type"{
 	instance_type = "t2.micro"
 	security_groups = [aws_security_group.SCG.id]
 	associate_public_ip_address = true
+	key_name = "access-key"
 	lifecycle {
 	 create_before_destroy = true
 	}
