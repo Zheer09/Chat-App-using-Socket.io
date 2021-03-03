88345e4f62c01741812cf81b040fc4ad0f9f1515
Zheer <bestzheer@gmail.com>
2021-03-03 15:48:49 +0300

    remove data for autoscalling

--- Application-loadbalancer.tf
+++ Application-loadbalancer.tf
@@ -5,11 +5,6 @@ resource "aws_lb_target_group" "Target-group"{
 	vpc_id = aws_vpc.main.id
 }
 
-data "aws_autoscaling_group" "Dynamic-EC2-instance"{
-	name = "Dynamic-EC2-instance"
-
-}
-
 resource "aws_autoscaling_attachment" "TG-attach" {
   autoscaling_group_name = aws_autoscaling_group.Dynamic-IN.id
   alb_target_group_arn   = aws_lb_target_group.Target-group.arn
